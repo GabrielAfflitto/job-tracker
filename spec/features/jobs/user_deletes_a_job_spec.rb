dd76d39f9fed755a587a9b68f28bd9fabd0db8df
Gabriel Afflitto <gafflitto92@gmail.com>
2017-12-12 07:59:29 -0700

    user can delete a job

---
+++ spec/features/jobs/user_deletes_a_job_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+  describe "User can delete a job" do
+    scenario "user can delete a job and redirect to company jobs page" do
+      company = Company.create!(name: "ESPN")
+      company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+      # company.jobs.create!(title: "QA Analyst", level_of_interest: 70, city: "New York City")
+
+      visit company_jobs_path(company.id)
+
+      expect(current_path).to eq(company_jobs_path(company.id))
+      expect(company.jobs.count).to eq(1)
+
+      click_link "Delete"
+
+      expect(current_path).to eq(company_jobs_path(company.id))
+      expect(company.jobs.count).to eq(0)
+    end
+  end
