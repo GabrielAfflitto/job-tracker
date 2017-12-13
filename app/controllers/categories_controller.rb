ecc1c29dc037f1ba8963760de326280dd42ea3b9
Gabriel Afflitto <gafflitto92@gmail.com>
2017-12-12 17:55:07 -0700

    Categories can be deleted

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -23,6 +23,13 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
   end
 
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+
+    redirect_to categories_path
+  end
+
   private
 
   def category_params
--- spec/features/categories/user_can_see_all_categories_spec.rb
+++ spec/features/categories/user_can_see_all_categories_spec.rb
@@ -10,11 +10,19 @@ describe "User can see all categories" do
     expect(page).to have_content("Art")
   end
 
-  it "each category has an edit and a link" do
+  it "each category has a delete link" do
     category_1 = Category.create!(title: "Music")
-    category_2 = Category.create!(title: "Art")
     visit categories_path
 
     expect(page).to have_content("Music")
+    expect(Category.count).to eq(1)
+
+    click_link "Delete"
+
+    expect(Category.count).to eq(0)
+  end
+
+  it "each category has an edit link" do
+    
   end
 end
